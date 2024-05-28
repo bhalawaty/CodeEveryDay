706e1f9c30586232ad88fba7143af9b17f7cf99a
bhalawaty <bilal.halawaty@gmail.com>
2024-05-28 23:58:06 +0300

    fix

--- src/topInterviewQuestions/easy/RemoveDuplicatesFromSortedArray.java
+++ src/topInterviewQuestions/easy/RemoveDuplicatesFromSortedArray.java
@@ -15,6 +15,7 @@ public class RemoveDuplicatesFromSortedArray {
         int j = 1;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
+
                 nums[j] = nums[i];
                 j++;
             }
