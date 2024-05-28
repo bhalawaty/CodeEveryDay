125ea35370841ffe56c278bd83d52160044fc58e
Bilal <bilal@bluworks.io>
2024-05-28 23:53:14 +0300

    solve RemoveDuplicatesFromSortedArray problem

---
+++ src/topInterviewQuestions/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,26 @@
+package topInterviewQuestions.easy;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,3,4,4,5,5};
+        int result = removeDuplicates(nums);
+        System.out.println("Indices: " + result);
+
+    }
+
+    /**
+     *<a href="https://leetcode.com/problems/remove-duplicates-from-sorted-array/">...</a>
+     */
+    public static int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
+
+
